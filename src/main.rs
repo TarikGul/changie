f1d8385aab1990af79d3a96822b1a727fe704bdc
tarikgul <tariksnow37@gmail.com>
2024-01-30 16:37:02 -0500

    refactor

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,9 @@ use reqwest::header::{HeaderMap, HeaderValue, CONTENT_TYPE, USER_AGENT};
 use reqwest::Url;
 use std::collections::hash_map::Entry;
 use std::collections::HashMap;
+use std::fs::{write, File};
+use std::io::{BufRead, BufReader, Error, Write};
+use std::path::Prefix;
 
 /// Simple program to greet a person
 #[derive(Parser, Debug)]
@@ -25,9 +28,138 @@ struct Args {
     repo: String,
     /// Number of times to greet
     #[arg(short, long)]
-    file: String,
+    file_path: String,
     #[arg(short, long)]
     target_version: String,
+}
+
+struct CommitMessageParts {
+    pr_num: String,
+    message: String,
+}
+
+#[derive(Debug)]
+struct SanitizedInfo {
+    commits: Vec<Commit>,
+    pub header: String,
+    pub body: String,
+}
+
+impl SanitizedInfo {
+    fn new(&mut self, args: Args, tag_name: &String) {
+        self.extract_commits();
+        let mut h: HashMap<String, Vec<String>> = HashMap::new();
+        for c in self.commits.iter() {
+            let prefix = self.extract_prefix(&c);
+            let m_parts = self.extract_message_parts(&c);
+            let sanitized_commit = self.create_commit(&args, &m_parts, &c);
+
+            match h.entry(prefix) {
+                Entry::Vacant(e) => {
+                    e.insert(vec![sanitized_commit]);
+                }
+                Entry::Occupied(mut e) => e.get_mut().push(sanitized_commit),
+            }
+        }
+
+        let mut body = String::from("");
+        let keys = h.keys().collect::<Vec<&String>>();
+        for k in keys {
+            let commit_header = "## ".to_owned() + &capitalize(k) + "\n\n";
+            let v = h.get(k).unwrap();
+            let joined_commits = v.join("\n") + "\n";
+            body = body + &commit_header + &joined_commits;
+        }
+
+        self.body = body;
+        self.header = self.create_release_header(tag_name, &args);
+    }
+
+    fn merge_changelog_contents(&self) -> String {
+        format!(
+            "{}\n {}",
+            &self.header,
+            &self.body
+        )
+    }
+
+    fn extract_commits(&mut self) {
+        let mut v: Vec<Commit> = Vec::new();
+
+        for commit in &self.commits {
+            // Once we reach the latest release commit we don't need to include it
+            if commit.commit.message.starts_with("chore(release)") {
+                break;
+            }
+            v.push(commit.clone());
+        }
+
+        self.commits = v
+    }
+
+    fn extract_prefix(&self, c: &Commit) -> String {
+        c.commit.message.split(":").collect::<Vec<_>>()[0]
+            .split("(")
+            .collect::<Vec<_>>()[0]
+            .to_owned()
+    }
+
+    fn extract_message_parts(&self, c: &Commit) -> CommitMessageParts {
+        let m = c.commit.message.lines().collect::<Vec<_>>()[0]
+            .split(" ")
+            .collect::<Vec<_>>();
+
+        CommitMessageParts {
+            message: m[0..m.len() - 1].join(" "),
+            pr_num: m.last().clone().unwrap().to_string(),
+        }
+    }
+
+    fn create_release_header(&self, tag_name: &String, args: &Args) -> String {
+        let date = chrono::offset::Local::now().to_string();
+        let parsed_tag = &tag_name[1..tag_name.len()];
+        let header = format!(
+            "## [{}](https://github.com/{}/{}/compare/{}..{})({})",
+            parsed_tag,
+            &args.org,
+            &args.repo,
+            &tag_name,
+            &args.target_version,
+            &date[0..10]
+        );
+
+        header
+    }
+
+    fn create_pr_link(&self, args: &Args, pr_num: &String) -> String {
+        format!(
+            "https://github.com/{}/{}/pull/{}",
+            &args.org,
+            &args.repo,
+            &pr_num[2..pr_num.len() - 1]
+        )
+    }
+
+    fn create_sha_link(&self, args: &Args, sha: &String) -> String {
+        format!(
+            "https://github.com/{}/{}/commit/{}",
+            &args.org, &args.repo, sha
+        )
+    }
+
+    fn create_commit(&self, args: &Args, m_parts: &CommitMessageParts, c: &Commit) -> String {
+        let pr_link = self.create_pr_link(&args, &m_parts.pr_num);
+        let pr_sha_link = self.create_sha_link(&args, &c.sha);
+
+        format!(
+            "- {} ([#{}]({})) ([{}]({}))",
+            m_parts.message,
+            &m_parts.pr_num[2..m_parts.pr_num.len() - 1],
+            pr_link,
+            &c.sha[0..6],
+            pr_sha_link
+        )
+    }
 }
 
 impl LatestRelease {
@@ -98,70 +230,6 @@ fn construct_headers() -> HeaderMap {
     headers
 }
 
-fn extract_commits(commits: Vec<Commit>) -> Vec<Commit> {
-    let mut v: Vec<Commit> = Vec::new();
-
-    for commit in commits {
-        // Once we reach the latest release commit we don't need to include it
-        if commit.commit.message.starts_with("chore(release)") {
-            break;
-        }
-        v.push(commit);
-    }
-
-    v
-}
-
-fn parse_commits(commits: Vec<Commit>, args: &Args) -> String {
-    let mut h: HashMap<&str, Vec<String>> = HashMap::new();
-    for c in commits.iter() {
-        let prefix = c.commit.message.split(":").collect::<Vec<_>>()[0]
-            .split("(")
-            .collect::<Vec<_>>()[0];
-        let v = c.commit.message.lines().collect::<Vec<_>>()[0]
-            .split(" ")
-            .collect::<Vec<_>>();
-        let message = v[0..v.len() - 1].join("");
-        let pr_num = v.last().copied().unwrap();
-        let pr_link = format!(
-            "https://github.com/{}/{}/pull/{}",
-            &args.org,
-            &args.repo,
-            &pr_num[2..pr_num.len() - 1]
-        );
-        let pr_sha_link = format!(
-            "https://github.com/{}/{}/commit/{}",
-            &args.org, &args.repo, c.sha
-        );
-        let key = format!(
-            "- {} ([#{}]({})) ([{}]({}))",
-            message,
-            &pr_num[2..pr_num.len() - 1],
-            pr_link,
-            &c.sha[0..6],
-            pr_sha_link
-        );
-
-        match h.entry(prefix) {
-            Entry::Vacant(e) => {
-                e.insert(vec![key]);
-            }
-            Entry::Occupied(mut e) => e.get_mut().push(key),
-        }
-    }
-
-    let mut body = String::from("");
-    let keys = h.keys().collect::<Vec<&&str>>();
-    for k in keys {
-        let commit_header = "## ".to_owned() + &capitalize(k) + "\n\n";
-        let v = h.get(k).unwrap();
-        let joined_commits = v.join("\n");
-        body = body + &commit_header + &joined_commits;
-    }
-
-    body
-}
-
 fn capitalize(s: &str) -> String {
     let mut c = s.chars();
     match c.next() {
@@ -170,34 +238,33 @@ fn capitalize(s: &str) -> String {
     }
 }
 
-fn create_release_header(tag_name: &String, args: &Args) -> String {
-    // ## [0.1.6](https://github.com/paritytech/asset-transfer-api/compare/v0.1.5..v0.1.6)(2024-01-22)
+fn write_to_file(path: &String, content: &String) -> Result<(), Error> {
+    let input = File::open(path)?;
+    let buffered = BufReader::new(input);
 
-    let date = chrono::offset::Local::now().to_string();
-    let parsed_tag = &tag_name[1..tag_name.len()];
-    let header = format!(
-        "## [{}](https://github.com/{}/{}/compare/{}..{})({})",
-        parsed_tag,
-        &args.org,
-        &args.repo,
-        &tag_name,
-        &args.target_version,
-        &date[0..10]
-    );
+    for line in buffered.lines() {
+        println!("{}", line?);
+    }
 
-    header
+    write(path, content)?;
+
+    Ok(())
 }
 
 #[tokio::main]
 async fn main() -> Result<(), ExitFailure> {
     let args = Args::parse();
     let res_commits = <Vec<Commit>>::get(&args.org, &args.repo).await;
-    let commits = res_commits.unwrap();
-    let extracted_commits = extract_commits(commits);
     let release_info = LatestRelease::get(&args.org, &args.repo).await;
+    let mut info = SanitizedInfo {
+        commits: res_commits.unwrap(),
+        header: "".to_string(),
+        body: "".to_string(),
+    };
+    SanitizedInfo::new(&mut info, args, &release_info.unwrap().tag_name);
+    let changelog_contents = SanitizedInfo::merge_changelog_contents(&info);
 
-    let body = parse_commits(extracted_commits, &args);
-    let header = create_release_header(&release_info.unwrap().tag_name, &args);
-
+    println!("{:?}", changelog_contents);
+    // let _ = write_to_file(&args.file_path, &content);
     Ok(())
 }
